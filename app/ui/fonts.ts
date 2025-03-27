830406a0c23ca9c873a015c55dbfafb12e53ab4f
Kevin Coronado <kevingalileo100@gmail.com>
2025-03-27 15:11:47 -0500

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+// ctrl + space en los corchetes y te salen todos los tipos de letras que hay en google fonts
+import { Montserrat, Lusitana } from "next/font/google";
+
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
